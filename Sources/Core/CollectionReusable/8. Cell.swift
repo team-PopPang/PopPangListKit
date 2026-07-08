8a8dc70876616f76fed70707ab835612a0d5e16a
indextrown <indextrown@gmail.com>
2026-07-08 15:57:42 +0900

    feat: cell에 ListingViewEventHandler 구현

--- Sources/Core/CollectionReusable/8. Cell.swift
+++ Sources/Core/CollectionReusable/8. Cell.swift
@@ -8,10 +8,16 @@
 import Foundation
 import DifferenceKit
 
-public struct Cell: Identifiable {
+public struct Cell: Identifiable, @MainActor ListingViewEventHandler {
     
+    /// Cell을 식별하기 위한 ID
     public let id: AnyHashable
+    
+    /// 셀을 구성하는 타입 소거된 Component
     public let component: AnyComponent
+    
+    /// 이벤트 저장소
+    let eventStorage = ListingViewEventStorage()
     
     public init(id: some Hashable, component: some Component) {
         self.id = id
@@ -37,5 +43,54 @@ extension Cell: Differentiable {
     
     public func isContentEqual(to source: Cell) -> Bool {
         self == source
+    }
+}
+
+// MARK: - Event Handler
+extension Cell {
+    
+    /// 셀이 선택되었을 때 호출되는 콜백을 등록합니다.
+    ///
+    /// - Parameters:
+    ///  - handler: 선택 이벤트 콜백
+    @MainActor
+    public func didSelect(_ handler: @escaping (DidSelectEvent.EventContext) -> Void) -> Self {
+        registerEvent(DidSelectEvent(handler: handler))
+    }
+    
+    /// 셀이 화면에 표시되기 직전에 호출되는 콜백을 등록합니다.
+    ///
+    /// - Parameters:
+    ///  - handler: willDisplay 이벤트 콜백
+    @MainActor
+    public func willDisplay(_ handler: @escaping (WillDisplayEvent.EventContext) -> Void) -> Self {
+        registerEvent(WillDisplayEvent(handler: handler))
+    }
+    
+    /// 셀이 화면에서 제거(사라짐)되었을 때 호출되는 콜백을 등록합니다.
+    ///
+    /// - Parameters:
+    ///  - handler: didEndDisplay 이벤트 콜백
+    @MainActor
+    public func didEndDisplay(_ handler: @escaping (DidEndDisplayingEvent.EventContext) -> Void) -> Self {
+        registerEvent(DidEndDisplayingEvent(handler: handler))
+    }
+    
+    /// 셀이 하이라이트(눌림 상태) 되었을 때 호출되는 콜백을 등록합니다.
+    ///
+    /// - Parameters:
+    ///  - handler: highlight 이벤트 콜백
+    @MainActor
+    public func onHighlight(_ handler: @escaping (HighlightEvent.EventContext) -> Void) -> Self {
+        registerEvent(HighlightEvent(handler: handler))
+    }
+    
+    /// 셀이 하이라이트 해제되었을 때 호출되는 콜백을 등록합니다.
+    ///
+    /// - Parameters:
+    ///  - handler: unhighlight 이벤트 콜백
+    @MainActor
+    public func onUnhighlight(_ handler: @escaping (UnhighlightEvent.EventContext) -> Void) -> Self {
+        registerEvent(UnhighlightEvent(handler: handler))
     }
 }
