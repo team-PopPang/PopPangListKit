58a0fe6d4f67282928e958348ff03fefbd4675c6
indextrown <indextrown@gmail.com>
2026-07-08 15:58:46 +0900

    feat: SupplementaryView에 ListingViewEventHandler 구현

--- Sources/Core/CollectionReusable/9. SupplementaryView.swift
+++ Sources/Core/CollectionReusable/9. SupplementaryView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-public struct SupplementaryView: Equatable {
+public struct SupplementaryView: Equatable, @MainActor ListingViewEventHandler {
     
     /// 보조 뷰의 종류(header, footer 등)
     public let kind: String
@@ -17,6 +17,9 @@ public struct SupplementaryView: Equatable {
     
     /// 보조 뷰의 정렬 위치
     public let alignment: NSRectAlignment
+    
+    /// 이벤트 저장소
+    let eventStorage = ListingViewEventStorage()
     
     public init(
         kind: String,
@@ -32,5 +35,27 @@ public struct SupplementaryView: Equatable {
         lhs.kind == rhs.kind &&
         lhs.component == rhs.component &&
         lhs.alignment == rhs.alignment
+    }
+}
+
+// MARK: - Event Handler
+extension SupplementaryView {
+    
+    /// 화면에 표시될 때 호출되는 콜백 등록
+    ///
+    /// - Parameters:
+    ///  - handler: 표시 이벤트 콜백
+    @MainActor
+    public func willDisplay(_ handler: @escaping (WillDisplayEvent.EventContext) -> Void) -> Self {
+        registerEvent(WillDisplayEvent(handler: handler))
+    }
+    
+    /// 화면에서 제거될 때 호출되는 콜백 등록
+    ///
+    /// - Parameters:
+    ///  - handler: 제거 이벤트 콜백
+    @MainActor
+    public func didEndDisplaying(_ handler: @escaping (DidEndDisplayingEvent.EventContext) -> Void) -> Self {
+        registerEvent(DidEndDisplayingEvent(handler: handler))
     }
 }
